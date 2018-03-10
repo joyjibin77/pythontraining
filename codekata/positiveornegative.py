6d68ba8c7a422d57d30651df57c5caf0a5e02a23
joyjibin77 <37235014+joyjibin77@users.noreply.github.com>
2018-03-10 03:40:53 -0500

    check number positive or negative

---
+++ codekata/positiveornegative.py
@@ -0,0 +1,11 @@
+
+x=int(input("please enter a number"))
+
+if x<0:
+    print("negative")
+elif x==0:
+    print("zero")
+elif x>100000:
+    print("enter correct value")
+else:
+    print("positive")
